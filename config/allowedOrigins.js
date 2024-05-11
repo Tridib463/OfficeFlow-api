4d4c34c3cf8c8d08ba22f5de0a0c68b2d33b4735
Tridib Chatterjee <tridibtech@gmail.com>
2024-05-11 09:12:52 +0530

    Ready to Deploy Backend

---
+++ config/allowedOrigins.js
@@ -0,0 +1,5 @@
+const allowedOrigins = [
+    'https://officeflow.onrender.com',
+]
+
+module.exports = allowedOrigins
---
+++ routes/authRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+const authController = require('../controllers/authController')
+const loginLimiter = require('../middleware/loginLimiter')
+
+router.route('/login')
+    .post(loginLimiter, authController.login)
+
+router.route('/signin')
+    .post(loginLimiter, authController.signup)
+
+router.route('/refresh')
+    .get(authController.refresh)
+
+router.route('/logout')
+    .post(authController.logout)
+
+module.exports = router
